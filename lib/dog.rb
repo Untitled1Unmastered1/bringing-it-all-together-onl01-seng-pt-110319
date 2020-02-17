4fd82b24b6e66ddfcfef6a26a91fd3eb4571d498
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2020-02-17 19:16:52 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -14,9 +14,9 @@ class Dog
       id INTEGER PRIMARY KEY,
       name TEXT,
       breed TEXT 
-        )
-        SQL
-        DB[:conn].execute(sql)
+    )
+    SQL
+    DB[:conn].execute(sql)
     end 
 
     def self.drop_table 
