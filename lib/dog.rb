f0c7fd3b12c70010661d7b98031300b4d2430bd5
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2020-02-17 19:17:17 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -17,50 +17,5 @@ class Dog
     )
     SQL
     DB[:conn].execute(sql)
-    end 
-
-    def self.drop_table 
-        sql = <<-SQL
-        DROP TABLE IF EXISTS dogs
-        SQL
-        DB[:conn].execute(sql)
-    end
-
-    def save 
-        if self.id 
-            self.update 
-        else 
-            sql = <<-SQL 
-            INSERT INTO dogs (name, breed)
-            VALUES (?, ?)
-            SQL
-            DB[:conn].execute(sql, self.name, self.breed)
-            @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs") [0] [0]
-            self 
-        end 
-    end 
-
-    def self.create(name:, breed:)
-        dog = Dog.new(name: name, breed: breed)
-        dog.save 
-        dog
-    end
-
-    def self.new_from_db(row)
-        id = row[0]
-        name = row[1]
-        breed = row[2]
-        self.new(name: name, breed: breed, id: id)
-    end
-
-    def self.find_by_id(name)
- 
-    def update 
-        sql = <<-SQL
-        UPDATE dogs
-        SET name = ?, breed = ?
-        WHERE id = ?
-        SQL
-        DB[:conn].execute(sql)
-    end
-end
+  end 
+end 
