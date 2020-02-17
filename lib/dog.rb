1b50c884f251ff9acc599c0e83f7c6822e626254
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2020-02-17 19:54:07 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -73,7 +73,14 @@ class Dog
       end
   end
   
-  def self.find_by_name 
+  def self.find_by_name(name)
+    sql = <<-SQL
+    SELECT * FROM dogs where name = ? LIMIT 1
+    SQL
+    DB[:conn].execute(sql, self.name).map do |row|
+      self.new_from_db(row)
+    end.first 
+  end 
       
   
   def update
