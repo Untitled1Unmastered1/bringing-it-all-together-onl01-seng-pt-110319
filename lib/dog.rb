8e41ed9128a616e5316544889307928ee3c21ed1
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2020-02-17 19:15:48 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,16 +1,16 @@
 require 'pry'
 class Dog 
-    attr_accessor :name, :breed, :id
+  attr_accessor :name, :breed, :id
 
-    def initialize(id: nil, name:, breed:)
-        @id = id 
-        @name = name 
-        @breed = breed 
-    end
+  def initialize(id: nil, name:, breed:)
+    @id = id 
+    @name = name 
+    @breed = breed 
+  end
 
-    def self.create_table 
-        sql = <<-SQL 
-        CREATE TABLE IF NOT EXISTS dogs (
+  def self.create_table 
+    sql = <<-SQL 
+    CREATE TABLE IF NOT EXISTS dogs (
             id INTEGER PRIMARY KEY,
             name TEXT,
             breed TEXT 
